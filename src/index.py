229859caa9a7cebb3128e6cf537f064ecdab4437
Felipe Ramos <felipevictor5120@gmail.com>
2025-10-23 12:05:27 -0300

    att

--- src/index.py
+++ src/index.py
@@ -1,33 +1,47 @@
 from bs4 import BeautifulSoup
-from scraping_functions import extrair_regiao, extrair_data, extrair_populacao,extrair_tipo, extrair_esgoto, extrair_doencas, extrair_obitos
+from scraping_functions import (extrair_regiao
+                                ,extrair_data
+                                ,extrair_fato
+                                ,extrair_tipo
+                                ,extrair_coleta_agua
+                                ,extrair_coleta_esgoto
+                                ,extrair_internacoes
+                                ,extrair_obitos)
 import requests
-
+import json
 
 class scraper:
-    def __init__(self):
-        self.info_area = {}
-
+    def __init__(self,output_file="./roraima.json"):
+        self.output_file = output_file
+        self.resultados = []
+        pass
+        
     def extrair_info(self):
-        for i in range(350010,355715,5):
+        for i in range(140004,140075,1):
             r = requests.get(f'https://www.painelsaneamento.org.br/explore/localidade?SE[l]={i}&page=1&ajax=ajax')
             soup = BeautifulSoup(r.content, 'lxml')
+            
+            info_area = {}
             regiao = extrair_regiao(soup)
-
             if regiao is not None:
-                self.info_area["data"] = extrair_data(soup)
-                self.info_area["populacao"] = extrair_populacao(soup)
-                self.info_area["area"] = extrair_tipo(soup)
-                self.extrair_indicadores(i)
-                print(f"🔍 Coleta do municipio {regiao} concluida!")
+                info_area["regiao"] = regiao
+                info_area["data"] = extrair_data(soup)
+                info_area["area"] = extrair_tipo(soup)
+                info_area["fato"] = extrair_fato(i)
+                self.extrair_indicadores(i,info_area)
+                print(f"🔍 Coleta do municipio {info_area["regiao"]} concluida!")
+                self.resultados.append(info_area)
+                self.salvar_resultado()
 
-    def extrair_indicadores(self,i):
-        self.info_area["doencas"] = extrair_doencas(i)
-        self.info_area["coleta_de_esgoto"] = extrair_esgoto(i)
-        self.info_area["incidenca_obitos_doenca_respiratoria"] = extrair_obitos(i)
-        self.info_area["valores"] = extrair_obitos(i)
-        print(self.info_area)
-
+    def extrair_indicadores(self,i,info_area):
+        info_area["internacoes"] = extrair_internacoes(i)
+        info_area["coleta_esgoto"] = extrair_coleta_esgoto(i)
+        info_area["coleta_agua"] = extrair_coleta_agua(i)
+        info_area["incidenca_obitos_doenca_respiratoria"] = extrair_obitos(i)
+        
+    def salvar_resultado(self):
+        with open(self.output_file,"w",encoding="utf-8") as f:
+            json.dump(self.resultados,f,ensure_ascii=False, indent=4)
 
 scraping = scraper()
 scraping.extrair_info()
-
